8d48a165dfea4570eda03925823bdc48f49420c7
mod hash <39630145+modhash@users.noreply.github.com>
2023-05-01 15:26:01 -0400

    Update Auto_Update.bat

--- Auto_Update.bat
+++ Auto_Update.bat
@@ -1,5 +1,5 @@
 @echo on
 cd C:\ProgramData\sysmon\
-@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/modhash/main/sysmonconfig-export.xml','C:\ProgramData\sysmon\sysmonconfig-export.xml')"
+@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/modhash/sysmon/main/sysmonconfig-export.xml','C:\ProgramData\sysmon\sysmonconfig-export.xml')"
 sysmon64.exe -c sysmonconfig-export.xml
 exit
